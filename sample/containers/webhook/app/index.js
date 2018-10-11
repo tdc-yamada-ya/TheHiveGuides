19fdbed1078868a2389377f8e452dbfae9729251
Yasuhiro Yamada <yamada_ya@tdc.co.jp>
2018-10-11 11:36:00 +0900

    サンプルプログラムを更新

---
+++ sample/containers/webhook/app/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const port = 3000
+
+app.use(bodyParser.json())
+
+app.post('/webhook', (req, res) => {
+  console.log(req.body)
+  res.send('OK')
+})
+
+app.listen(port, () => {
+  console.log(`Webhook app listening on port ${port}.`)
+})
